8accf0ee63e0b70d7282984d70cd88afc4e12f29
Brian J Smith <brian-j-smith@uiowa.edu>
2020-01-24 08:09:01 -0600

    Initial package commit

---
+++ R/testing.R
@@ -0,0 +1,38 @@
+#' Test Functions
+#'
+#' Functions to demonstrate R CMD check warning produced by registration of a
+#' formula method when it and its S3 generic both have an ellipsis as the first
+#' argument.
+#' 
+#' @param ... arguments passed to other methods.
+#'
+#' @rdname combine
+#' 
+combine <- function(...) UseMethod("combine")
+
+#' @rdname combine
+combine.default <- function(...) c(...)
+
+#' @rdname combine
+combine.data.frame <- function(...) c(...)
+
+#' @rdname combine
+combine.factor <- function(...) c(...)
+
+#' @rdname combine
+combine.formula <- function(...) c(...)
+
+#' @rdname combine
+combine.list <- function(...) c(...)
+
+#' @rdname combine
+combine.logical <- function(...) c(...)
+
+#' @rdname combine
+combine.matrix <- function(...) c(...)
+
+#' @rdname combine
+combine.numeric <- function(...) c(...)
+
+
+c.formula <- function(...) c(...)
